7b22c125a2337c87d8cef416c46c3f57e3288b0b
konstantin.korovin <cosmo.stan@yandex.com>
2021-01-20 20:25:33 +0300

    add first test

---
+++ __tests__/test.genDiff.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import genDiff from '../genDiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const file1 = getFixturePath('file1.json');
+const file2 = getFixturePath('file2.json');
+const expected = readFile('expected_file');
+
+test('compare 2 files', () => {
+  expect(genDiff(file1, file2)).toBe(expected);
+});
