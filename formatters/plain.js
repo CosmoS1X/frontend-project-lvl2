ba139c33381d6e42b898d56056c03cc9e7090ff2
konstantin.korovin <cosmo.stan@yandex.com>
2021-02-03 20:57:54 +0300

    add plain formatter

---
+++ formatters/plain.js
@@ -0,0 +1,43 @@
+/* eslint-disable object-curly-newline */
+const checkValue = (data) => {
+  if (data === undefined || data === null) {
+    return data;
+  }
+
+  switch (typeof data) {
+    case 'object':
+      return '[complex value]';
+    case 'string':
+      return `'${data}'`;
+    default:
+      return data;
+  }
+};
+
+const buildStr = (data) => {
+  const iter = (content, acc, ancestry) => {
+    const { name, value, status, children, oldValue } = content;
+    const newAncestry = `${ancestry}.${name}`;
+    const pattern = `${acc}Property '${newAncestry.slice(1)}'`;
+
+    switch (status) {
+      case 'hasChildren':
+        return children.flatMap((child) => iter(child, acc, newAncestry));
+      case 'added':
+        return `${pattern} was added with value: ${checkValue(value)}`;
+      case 'deleted':
+        return `${pattern} was removed`;
+      case 'changed':
+        return `${pattern} was updated. From ${checkValue(oldValue)} to ${checkValue(value)}`;
+      default:
+        return acc;
+    }
+  };
+
+  return [iter(data, '', '')]
+    .flat()
+    .filter((item) => item !== '')
+    .join('\n');
+};
+
+export default (data) => data.reduce((acc, item) => `${acc}${buildStr(item)}\n`, '').trimEnd();
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import getData from './src/parsers.js';
 import diff from './src/diff.js';
 import stylish from './formatters/stylish.js';
+import plain from './formatters/plain.js';
 
 export default (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = getData(filepath1);
@@ -9,6 +10,9 @@ export default (filepath1, filepath2, formatName = 'stylish') => {
 
   if (formatName === 'stylish') {
     return stylish(result);
+  }
+  if (formatName === 'plain') {
+    return plain(result);
   }
 
   return `${formatName} is unknown formatter`;
