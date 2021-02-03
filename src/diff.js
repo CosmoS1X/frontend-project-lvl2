fea696cb10d0f3f486eaf6c491c8f50a282dda47
konstantin.korovin <cosmo.stan@yandex.com>
2021-02-03 21:56:40 +0300

    refactor src/diff.js

--- src/diff.js
+++ src/diff.js
@@ -1,39 +1,32 @@
+/* eslint-disable object-curly-newline */
 import _ from 'lodash';
 
 const diff = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
-  const commonKeys = _.uniq(_.concat(keys1, keys2)).sort();
+  const commonKeys = _.uniq(_.concat(keys1, keys2));
 
-  const result = commonKeys.reduce((acc, key) => {
-    const fileOneHasKey = _.has(data1, key);
-    const fileTwoHasKey = _.has(data2, key);
+  return commonKeys
+    .sort()
+    .reduce((acc, key) => {
+      if ((_.has(data1, key) && typeof data1[key] === 'object') && (_.has(data2, key) && typeof data2[key] === 'object')) {
+        acc.push({ name: key, status: 'hasChildren', children: diff(data1[key], data2[key]) });
+      }
+      if (_.has(data1, key) && _.has(data2, key) && (typeof data1[key] !== 'object' || typeof data2[key] !== 'object')) {
+        const check = data1[key] === data2[key]
+          ? { name: key, value: data1[key], status: 'unchanged' }
+          : { name: key, value: data2[key], status: 'changed', oldValue: data1[key] };
+        acc.push(check);
+      }
+      if (_.has(data1, key) && !_.has(data2, key)) {
+        acc.push({ name: key, value: data1[key], status: 'deleted' });
+      }
+      if (!_.has(data1, key) && _.has(data2, key)) {
+        acc.push({ name: key, value: data2[key], status: 'added' });
+      }
 
-    if ((fileOneHasKey && typeof data1[key] === 'object') && (fileTwoHasKey && typeof data2[key] === 'object')) {
-      acc.push({ name: key, status: 'hasChildren', children: diff(data1[key], data2[key]) });
-    }
-    if (fileOneHasKey && fileTwoHasKey && (typeof data1[key] !== 'object' || typeof data2[key] !== 'object')) {
-      const check = data1[key] === data2[key]
-        ? { name: key, value: data1[key], status: 'unchanged' }
-        : {
-          name: key,
-          value: data2[key],
-          status: 'changed',
-          oldValue: data1[key],
-        };
-      acc.push(check);
-    }
-    if (fileOneHasKey && !fileTwoHasKey) {
-      acc.push({ name: key, value: data1[key], status: 'deleted' });
-    }
-    if (!fileOneHasKey && fileTwoHasKey) {
-      acc.push({ name: key, value: data2[key], status: 'added' });
-    }
-
-    return acc;
-  }, []);
-
-  return result;
+      return acc;
+    }, []);
 };
 
 export default diff;
