8e0c66fa1fb533d7422f8d3212fa142819b11850
konstantin.korovin <cosmo.stan@yandex.com>
2021-02-03 21:10:48 +0300

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1>')
   .arguments('<filepath2>')
-  .option('-f, --format [type]', 'output format', 'plain')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2, program.opts().format));
   })
