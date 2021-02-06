407c5e50d35037a75bae162e8c68a09c92c4f042
konstantin.korovin <cosmo.stan@yandex.com>
2021-02-06 23:36:12 +0300

    small fix diff.js

--- src/diff.js
+++ src/diff.js
@@ -5,7 +5,7 @@ const diff = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
   const commonKeys = _.uniq([...keys1, ...keys2]);
-  const sorted = commonKeys.slice().sort();
+  const sorted = _.sortBy(commonKeys);
 
   return sorted.map((key) => {
     switch (true) {
