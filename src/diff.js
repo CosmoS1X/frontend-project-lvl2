6bf454bedcad774ac4c6a3f49510621f82d3bcc3
konstantin.korovin <cosmo.stan@yandex.com>
2021-02-07 19:42:44 +0300

    update diff.js

--- src/diff.js
+++ src/diff.js
@@ -5,13 +5,13 @@ const diff = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
   const commonKeys = _.uniq([...keys1, ...keys2]);
-  const sorted = _.sortBy(commonKeys);
+  const sortedKeys = _.sortBy(commonKeys);
 
-  return sorted.map((key) => {
+  return sortedKeys.map((key) => {
     switch (true) {
-      case (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])):
+      case (typeof data1[key] === 'object' && typeof data2[key] === 'object'):
         return { name: key, status: 'hasChildren', children: diff(data1[key], data2[key]) };
-      case _.isEqual(data1[key], data2[key]):
+      case data1[key] === data2[key]:
         return { name: key, value: data1[key], status: 'unchanged' };
       case _.has(data1, key) && _.has(data2, key):
         return { name: key, value: data2[key], status: 'changed', oldValue: data1[key] };
