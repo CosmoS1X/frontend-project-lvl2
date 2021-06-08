817d96ee5c4fb927a3176c71546dac00e4fc7a54
konstantin.korovin <cosmo.stan@yandex.com>
2021-06-08 17:23:56 +0300

    refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,56 +1,36 @@
 import fs from 'fs';
-import { fileURLToPath } from 'url';
 import path from 'path';
-import genDiff from '../formatters/index.js';
+import genDiff from '../index.js';
 
-const getFixturePath = (filename) => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  return path.join(__dirname, '..', '__fixtures__', filename);
-};
+const getFixturePath = (filename) => path.join('__fixtures__', filename);
 
-const jsonF1 = getFixturePath('flat1.json');
-const jsonF2 = getFixturePath('flat2.json');
-const ymlF1 = getFixturePath('flat1.yml');
-const ymlF2 = getFixturePath('flat2.yml');
-const expectedF = fs.readFileSync(getFixturePath('expected_flat'), 'utf-8');
+const json1 = getFixturePath('file1.json');
+const json2 = getFixturePath('file2.json');
+const yml1 = getFixturePath('file1.yml');
+const yml2 = getFixturePath('file2.yml');
+const expectedStylish = fs.readFileSync(getFixturePath('stylish.txt'), 'utf-8');
+const expectedPlain = fs.readFileSync(getFixturePath('plain.txt'), 'utf-8');
+const expectedJSON = fs.readFileSync(getFixturePath('json.txt'), 'utf-8');
 
-test('compare flat .json files', () => {
-  expect(genDiff(jsonF1, jsonF2)).toBe(expectedF);
+const tests = [
+  [json1, json2, 'stylish', expectedStylish],
+  [yml1, yml2, 'stylish', expectedStylish],
+  [json1, json2, 'plain', expectedPlain],
+  [yml1, yml2, 'plain', expectedPlain],
+  [json1, json2, 'json', expectedJSON],
+  [yml1, yml2, 'json', expectedJSON],
+];
+
+test('Compare files with stylish format by default', () => {
+  expect(genDiff(json1, json2)).toBe(expectedStylish);
 });
 
-test('compare flat .yml files', () => {
-  expect(genDiff(ymlF1, ymlF2)).toBe(expectedF);
+test('If formatter is unknown', () => {
+  expect(() => genDiff(yml1, yml2, 'test')).toThrow();
 });
 
-const jsonR1 = getFixturePath('recursive1.json');
-const jsonR2 = getFixturePath('recursive2.json');
-const ymlR1 = getFixturePath('recursive1.yml');
-const ymlR2 = getFixturePath('recursive2.yml');
-const expectedR = fs.readFileSync(getFixturePath('expected_recursive'), 'utf-8');
-const expectedP = fs.readFileSync(getFixturePath('expected_plain'), 'utf-8');
-const expectedJ = fs.readFileSync(getFixturePath('expected_json.json'), 'utf-8');
-
-test('compare recursive .json files with stylish format', () => {
-  expect(genDiff(jsonR1, jsonR2, 'stylish')).toBe(expectedR);
-});
-
-test('compare recursive .yml files with stylish format by default', () => {
-  expect(genDiff(ymlR1, ymlR2)).toBe(expectedR);
-});
-
-test('compare recursive .json files with plain format', () => {
-  expect(genDiff(jsonR1, jsonR2, 'plain')).toBe(expectedP);
-});
-
-test('compare recursive .yml files with plain format', () => {
-  expect(genDiff(ymlR1, ymlR2, 'plain')).toBe(expectedP);
-});
-
-test('compare recursive .json files with json format', () => {
-  expect(genDiff(jsonR1, jsonR2, 'json')).toBe(JSON.stringify(JSON.parse(expectedJ)));
-});
-
-test('if formatter is unknown', () => {
-  expect(() => genDiff(ymlR1, ymlR2, 'test')).toThrow();
+describe.each(tests)('Test each formatter', (file1, file2, formatter, expected) => {
+  test(`Compare ${path.extname(file1)} files with ${formatter} format`, () => {
+    expect(genDiff(file1, file2, formatter)).toBe(expected);
+  })
 });
